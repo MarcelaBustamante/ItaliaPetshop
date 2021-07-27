6577a7089b50b1577be557759dd171170934b95a
mbustamante <mbustamante@medallia.com>
2021-07-27 00:37:32 -0300

    cambio de enfoque

---
+++ src/components/Navbar/Sidebar.svelte
@@ -0,0 +1,44 @@
+<script>
+  import links from "../../constants/links";
+  import { link } from "svelte-routing";
+  import Login from "../../pages/Login.svelte";
+  import globalStore from "../../stores/globalStore";
+  import {fly,fade} from "svelte/transition"
+</script>
+
+<div class="sidebar-container" transition:fly ={{ x:-1000 }}>
+  <div class="sidebar">
+    <div class="sidebar-header" transition:fade={{delay: 400}}>
+      <button
+        class="btn-close"
+        on:click={() => {
+          globalStore.toggleItem("sidebar", false);
+        }}
+      >
+        <i class="fas fa-window-close" />
+      </button>
+    </div>
+    <!-- site logo -->
+    <img
+      src="/assets/images/logo.svg"
+      class="logo sidebar-logo"
+      alt="italias logo"
+    />
+    <!-- links -->
+    <ul class="sidebar-links">
+      {#each links as sideLink}
+        <li>
+          <a
+            href={sideLink.url}
+            use:link
+            on:click={() => {
+              globalStore.toggleItem("sidebar", false);
+            }}
+          >
+            {sideLink.text}</a
+          >
+        </li>
+      {/each}
+    </ul>
+  </div>
+</div>
