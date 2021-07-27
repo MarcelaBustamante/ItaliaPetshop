ce2af5c97e4cca46d104986db5b2258980bbd302
mbustamante <mbustamante@medallia.com>
2021-07-27 01:04:19 -0300

    app

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script>
   //pages
   import Login from "./pages/Login.svelte";
-  import Checkout from "./pages/Checkout.svelte";
   import ProductTemplate from "./pages/ProductTemplate.svelte";
   import Products from "./pages/Products.svelte";
   import Home from "./pages/Home.svelte";
